bdf2cb659f86022ccaf57a00dcafb548f216194d
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2023-09-12 22:26:31 +0900

    add French

--- hello.go
+++ hello.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
 
 func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
+    if name == "" {
+        name = "World"
+    }
 
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+    prefix := englishHelloPrefix
+
+    switch language {
+    case french:
+        prefix = frenchHelloPrefix
+    case spanish:
+        prefix = spanishHelloPrefix
+    }
+
+    return prefix + name
 }
 
 func main() {
